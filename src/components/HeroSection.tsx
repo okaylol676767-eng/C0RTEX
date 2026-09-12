df079d92d4da7c96d47004240e827a2dc5a105ae
okaylol676767-eng <okaylol676767-eng@users.noreply.github.com>
2026-09-13 00:01:22 +0530

    fix: nudge finding cluster a little further right

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -77,7 +77,7 @@ export function HeroSection() {
 
       {/* Finding ticker + status pill: clustered under the REPORTS nav item */}
       <div
-        className="absolute left-1/2 top-24 z-20 hidden -translate-x-[80px] lg:block"
+        className="absolute left-1/2 top-24 z-20 hidden -translate-x-[30px] lg:block"
         onMouseEnter={() => {
           setLookTarget("threat");
           setStatus("PATH DETECTED");
